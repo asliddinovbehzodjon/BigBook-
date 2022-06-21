d8cdcfecb5153443e6b963c14dd8b5b30f52784b
Behzod Asliddinov <behzodme@gmail.com>
2022-06-21 11:53:48 +0500

    Final Part

--- bigbook/settings.py
+++ bigbook/settings.py
@@ -20,7 +20,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
     'http://127.0.0.1:8000',
     'http://127.0.0.1:8080',
-    'http://bigbookuz.pythonanywhere.com/'
+    # 'http://bigbookuz.pythonanywhere.com/'
 ]
 # Application definition
 
