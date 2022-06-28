4d3dae524cea1b25c54b8b27e662785ce169577a
Behzod Asliddinov <behzodme@gmail.com>
2022-06-28 10:27:51 +0500

    books

--- books/serializer.py
+++ books/serializer.py
@@ -15,7 +15,7 @@ class GenresSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Genres
         fields = ['url','id','name','books']
-        depth=1
+        depth=2
 class BookSerializer(serializers.ModelSerializer):
     # uploader = serializers.StringRelatedField()
     comments = CommentSerializer(read_only=True,many=True)
