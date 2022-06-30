3bf5d9eb037b17d41c698d01745b7c1dab3f3994
Behzod Asliddinov <behzodme@gmail.com>
2022-06-30 11:15:09 +0500

    netlifypublish

--- bigbook/settings.py
+++ bigbook/settings.py
@@ -17,7 +17,8 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
-    'http://127.0.0.1:8080'
+    'http://127.0.0.1:8080',
+    'https://bigbookuz.netlify.app/'
 ]
 # Application definition
 
