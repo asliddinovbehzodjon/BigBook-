31c921d823d33a3dec1251123d3d9daabc0efb41
Behzod Asliddinov <behzodme@gmail.com>
2022-07-21 20:12:30 +0500

    behzod

--- readers/views.py
+++ readers/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
 # Create your views here.
+from rest_framework import status
 from .models import Kitobxon,BotUsers
 from .serializer import KitobxonSerializer,BotUserSerializer
 from rest_framework.viewsets import ModelViewSet
@@ -10,3 +11,7 @@ class AllReaders(ModelViewSet):
 class BotUsersAll(ModelViewSet):
     queryset = BotUsers.objects.all()
     serializer_class  = BotUserSerializer
+    def perform_create(self, serializer):
+        if self.queryset.filter(telegram_id = self.request.telegram_id).exists():
+            return Response(status=status.HTTP_208_ALREADY_REPORTED)
+        return super().perform_create(serializer)
