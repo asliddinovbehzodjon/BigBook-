9450dfc0104a5786215b0261aa6a1130f4fd60a0
Behzod Asliddinov <behzodme@gmail.com>
2022-07-21 19:55:40 +0500

    behzod

--- books/urls.py
+++ books/urls.py
@@ -1,12 +1,13 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import  path,include
 from .views import *
-from readers.views import AllReaders
+from readers.views import AllReaders,BotUsersAll
 router = DefaultRouter()
 router.register('genres',AllGenres)
 router.register('books',AllBooks)
 router.register('readers',AllReaders)
 router.register('comments',CommentWrite)
+router.register('botusers',BotUsersAll)
 urlpatterns = [
     path('',include(router.urls)),
     path('more',MoreDownloaded.as_view()),
