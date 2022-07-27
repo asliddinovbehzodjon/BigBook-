b2db505a4ec33a915d2d433aaa495d8d4e01b2a3
Behzod <behzodme@gmail.com>
2022-07-27 08:21:02 +0500

    behzod

--- books/views.py
+++ books/views.py
@@ -27,7 +27,7 @@ class BasicPagination(PageNumberPagination):
             'results': data
         })
 class CustomeBasicPagination(PageNumberPagination):
-    page_size = 5
+    page_size = 3
     def get_paginated_response(self, data):
         return Response({
         
