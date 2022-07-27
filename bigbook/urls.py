e02cf5a8327ad41fe3dc34f76a4b449fcd8f6004
Behzod <behzodme@gmail.com>
2022-07-27 14:59:57 +0500

    behzod

--- bigbook/urls.py
+++ bigbook/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('admin/', admin.site.urls),
+    path('behzod/owner', admin.site.urls),
     path('api/v1/',include('books.urls')),
     path('api/v1/',include('djoser.urls')),
     path('api/v1/',include('djoser.urls.authtoken')),
